a10477f7e1ef1ee4688900c006cf400e928147d6
Jonathan Lamb <jlamb@polysync.io>
2018-05-14 12:20:33 -0700

    Generates app src/lib.rs

--- src/new_cmd.rs
+++ src/new_cmd.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
 use std::process::Command;
 
 use super::{run_cmd, Error};
@@ -6,11 +9,7 @@ use config::Config;
 pub fn handle_new_cmd(config: &Config) -> Result<(), Error> {
     // TODO
     //
-    // name fel4 or feL4?
     // name/path, subcommands for context help?
-    //
-    // cargo new --lib
-
     let package_name = "fel4-project";
 
     // base cargo new command to construct project scaffolding
@@ -29,6 +28,32 @@ pub fn handle_new_cmd(config: &Config) -> Result<(), Error> {
 
     // build a bare library project
     run_cmd(cmd.arg("--lib").arg("fel4-project"))?;
+
+    // create/truncate the generated src/lib.rs with our new contents
+    let mut lib_src_file = File::create(Path::new(package_name).join("src").join("lib.rs"))?;
+
+    lib_src_file.write_all(
+        b"#![no_std]
+extern crate sel4_sys;
+
+use sel4_sys::DebugOutHandle;
+
+macro_rules! print {
+    ($($arg:tt)*) => ({
+        use core::fmt::Write;
+        DebugOutHandle.write_fmt(format_args!($($arg)*)).unwrap();
+    });
+}
+
+macro_rules! println {
+    ($fmt:expr) => (print!(concat!($fmt, \"\\n\")));
+    ($fmt:expr, $($arg:tt)*) => (print!(concat!($fmt, \"\\n\"), $($arg)*));
+}
+
+pub fn run() {
+    println!(\"\\nhello from a fel4 app!\\n\");
+}",
+    )?;
 
     Ok(())
 }
